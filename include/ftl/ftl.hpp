200575d908bd22cdd21de1e7f0d6e78b6cc2a051
Lukasz Gut <blinku.dev@gmail.com>
2020-07-10 11:30:45 +0200

    Update CMakeLists.txt

--- include/ftl/ftl.hpp
+++ include/ftl/ftl.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 namespace ftl {
 
 /// \brief Factorial
@@ -10,6 +12,10 @@ namespace ftl {
 constexpr unsigned int factorial(unsigned int number)
 {
   return number <= 1 ? number : factorial(number - 1) * number;
+}
+
+constexpr std::optional<int> get_optional() {
+  return {5};
 }
 
 int fun();
