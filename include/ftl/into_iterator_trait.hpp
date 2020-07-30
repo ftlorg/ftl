e1e791c3d2bfb27f07bab8ad058d6294eed43487
Łukasz Gut <40406905+Blinkuu@users.noreply.github.com>
2020-07-30 12:52:41 +0200

    Add docs for into_iterator_trait (#5)
    * Add documentation for into_iterator_trait.hpp
    
    * Add documentation to iterator_interface
    
    * Remove redundant @brief
    
    * Add more documentation

--- include/ftl/into_iterator_trait.hpp
+++ include/ftl/into_iterator_trait.hpp
@@ -2,6 +2,13 @@
 
 namespace ftl {
 
+/**
+ * This trait must be specialized for data types
+ * that can be converted into an iterator.
+ *
+ * @tparam T The type which is being converted to an iterator.
+ * @tparam U The type of an iterator suitable for type T.
+ */
 template<typename T, typename U>
 struct into_iterator_trait {
   [[nodiscard]] constexpr static auto into_iter(T &) -> U {
