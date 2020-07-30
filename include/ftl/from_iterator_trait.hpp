8b54d0ce3db5c9110637791df69e102f73eb4102
Lukasz Gut <blinku.dev@gmail.com>
2020-07-30 12:50:32 +0200

    Add docs for from_iterator_trait

--- include/ftl/from_iterator_trait.hpp
+++ include/ftl/from_iterator_trait.hpp
@@ -2,6 +2,13 @@
 
 namespace ftl {
 
+/**
+ * This trait must be specialized for iterator types
+ * that can be converted into a collection.
+ *
+ * @tparam T The iterator type which is being converted to a collection.
+ * @tparam U The type of a collection to which we want to convert from a given T.
+ */
 template<typename T, typename U>
 struct from_iterator_trait {
   [[nodiscard]] constexpr static auto from_iter(const T &iter) -> U {
