cc758684ae6852b7f25675cd16e714fcabc5661d
Lukasz Gut <blinku.dev@gmail.com>
2020-08-14 12:09:01 +0200

    Fix compilation error

--- tests/test_enumerate_iterator.cpp
+++ tests/test_enumerate_iterator.cpp
@@ -63,7 +63,7 @@ TEST_CASE(TEST_TAG "enumerate with side effects", TEST_TAG) {
   constexpr std::size_t size = 5;
   ftl::array<int, size> arr = { 1, 2, 3, 4, 5 };
 
-  for (const auto &[index, item] : arr.iter().enumerate()) { item = size - index; }
+  for (const auto &[index, item] : arr.iter().enumerate()) { item = static_cast<int>(size - index); }
 
   REQUIRE(arr == ftl::array<int, size>{ 5, 4, 3, 2, 1 });
 }
