cb4ec10626422b1d14532251b55ddc5b78e0145d
Lukasz Gut <blinku.dev@gmail.com>
2020-08-31 14:37:19 +0200

    Remove warning

--- tests/test_inspect_iterator.cpp
+++ tests/test_inspect_iterator.cpp
@@ -7,14 +7,13 @@ TEST_CASE(TEST_TAG "inspect map", TEST_TAG) {
   ftl::list<int> arr = { 1, 2, 3, 4, 5 };
 
   auto mapped_arr = arr.iter()
-                      .inspect([](const auto &x) { INFO("Inspecting x = " << x); })
+                      .inspect([](const auto &x) { INFO("Inspecting x = " << x) })
                       .map([](const auto &x) { return x * x; })
                       .collect<ftl::list<int>>();
 
   // Inspect doesn't allow for side effects. This doesn't compile.
-  // [[maybe_unused]] auto mapped_arr2
-  //   = arr.iter().inspect([](auto &x) { x = 3; }).map([](const auto &x) { return x * x; }).collect<ftl::array<int,
-  //   size>>();
+  //[[maybe_unused]] auto mapped_arr2
+  //  = arr.iter().inspect([](auto &x) { x = 3; }).map([](const auto &x) { return x * x; }).collect<ftl::list<int>>();
 
   REQUIRE(mapped_arr == ftl::list<int>{ 1, 4, 9, 16, 25 });
 }
@@ -23,7 +22,7 @@ TEST_CASE(TEST_TAG "inspect map const", TEST_TAG) {
   const ftl::list<int> arr = { 1, 2, 3, 4, 5 };
 
   auto mapped_arr = arr.iter()
-                      .inspect([](const auto &x) { INFO("Inspecting x = " << x); })
+                      .inspect([](const auto &x) { INFO("Inspecting x = " << x) })
                       .map([](const auto &x) { return x * x; })
                       .collect<ftl::list<int>>();
 
