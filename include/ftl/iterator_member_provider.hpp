0a2935f4c8014afd60d7759c9a9b54e4431eb8db
bigbadel <greg090912@gmail.com>
2020-10-11 17:22:46 +0200

    Fix typo

--- include/ftl/iterator_member_provider.hpp
+++ include/ftl/iterator_member_provider.hpp
@@ -102,12 +102,12 @@ struct iterator_member_provider<Iter, std::bidirectional_iterator_tag>
   : public iterator_member_provider<Iter, std::forward_iterator_tag> {
   auto operator--() const -> const Iter & {
    --static_cast<const Iter &>(*this).iterator_;
-    return --static_cast<const Iter &>(*this);
+    return static_cast<const Iter &>(*this);
   }
 
   auto operator--() -> Iter & {
     --static_cast<Iter &>(*this).iterator_;
-    return --static_cast<Iter &>(*this);
+    return static_cast<Iter &>(*this);
   }
 
   auto operator--(int) -> Iter {
