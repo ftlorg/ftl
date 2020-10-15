8b95dd2e044f9b6c5eb12dad80c889d5638f765c
bigbadel <greg090912@gmail.com>
2020-10-15 12:35:27 +0200

    Add license to header files

--- include/ftl/set.hpp
+++ include/ftl/set.hpp
@@ -1,3 +1,9 @@
+
+// Copyright Grzegorz Litarowicz and £ukasz Gut 2020 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <set>
